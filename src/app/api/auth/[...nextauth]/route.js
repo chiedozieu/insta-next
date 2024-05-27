aee8680838317a3d09e154ace65b7b233ebf0bb1
chiedozieu <chiedozieugwor@gmail.com>
2024-05-27 17:28:06 +0100

    next-auth integration

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,16 @@
+// in the app   api/auth/[...next-auth/route.js]import NextAuth from "next-auth"
+import NextAuth from "next-auth" 
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth ({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.Google_ID,
+      clientSecret: process.env.Google_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
+
+export { handler as GET , handler as POST};    
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,43 @@
+'use client';
+
 import Link from "next/link"
 import Image from "next/image"
+import { signIn, useSession, signOut } from "next-auth/react"
 
 export default function Header() {
+    const { data: session } = useSession(); 
+    console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 z-30 p-3">
-    <div className="flex items-center justify-between max-w-6xl mx-auto">
-        <Link href='/' className="hidden lg:inline-flex">
-         <Image
-         src='/Instagram.png'
-         width={66} height={66}
-         alt="Instagram Logo"
-          />
-        </Link>
-        <Link href='/' className="lg:hidden">
-         <Image
-         src='/Insta.png'
-         width={50} height={50}
-         alt="Instagram Logo"
-          />
-        </Link>
-        <input type="text" placeholder="Search..."
-        className="bg-gray-50 border border-gray-200 text-sm rounded w-full py-2 px-4 max-w-[210px]" />
-        <button className="font-semibold text-blue-500">Sign In</button>
-    </div>
+        <div className="flex items-center justify-between max-w-6xl mx-auto">
+            <Link href='/' className="hidden lg:inline-flex">
+            <Image
+            src='/Instagram.png'
+            width={66} height={66}
+            alt="Instagram Logo"
+            />
+            </Link>
+            <Link href='/' className="lg:hidden">
+            <Image
+            src='/Insta.png'
+            width={50} height={50}
+            alt="Instagram Logo"
+            />
+            </Link>
+            <input type="text" placeholder="Search..."
+            className="bg-gray-50 border border-gray-200 text-sm rounded w-full py-2 px-4 max-w-[210px]" />
+
+            { session ? (
+                <img src={session.user.image} alt={session.user.name} onClick={signOut} className="h-10 w-10 rounded-full cursor-pointer"/>
+            ) : (
+            <button 
+            className="font-semibold text-blue-500"
+            onClick={signIn}
+            >Sign In
+            </button>
+                
+            ) }
+        </div>
     </div>
   )
 }
